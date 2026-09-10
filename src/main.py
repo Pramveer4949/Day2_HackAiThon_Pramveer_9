15db71e9df0bd719730456b7025cb9ab44b03b43
Pramveer4949 <mrpramveer4949@gmail.com>
2026-09-10 20:36:59 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,15 +6,16 @@ print("=================================")
 x=1
 v=False
 while x<=5 :
-  z=int(input("Guess number between(1,50)"))
+  z=int(input("Guess number between(1,50):-"))
   if z==k :
-    print("Congratulation You have guessed The number")
+    print("Congratulation!! You have guessed The number")
     v=True
     break
   elif z>k:
     print("Think Lower ")
   elif z<k:
     print("Think Higher")
+  x+=1
 if v==False :
   print(f"The Number Was {k}")
     
